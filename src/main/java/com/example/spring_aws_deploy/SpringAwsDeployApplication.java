c23b9791c01aa148287370854b37b29308d558ea
EC2 Default User <ec2-user@ip-172-31-35-155.ap-south-1.compute.internal>
2023-10-17 14:39:45 +0000

    IC

---
+++ src/main/java/com/example/spring_aws_deploy/SpringAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsDeployApplication.class, args);
+	}
+
+}
