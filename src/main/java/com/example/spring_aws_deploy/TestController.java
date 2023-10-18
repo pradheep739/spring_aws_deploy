6540bc4c8e80437293fcacdc2ee737c98631597f
pradheep739 <118267473+pradheep739@users.noreply.github.com>
2023-10-18 18:44:44 +0530

    Update TestController.java
    Typo RequestMappingquestMapping it is chnged to RequestMapping

--- src/main/java/com/example/spring_aws_deploy/TestController.java
+++ src/main/java/com/example/spring_aws_deploy/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMappingquestMapping("/demo")
+@RequestMapping("/demo")
 public class TestController {
 
         @GetMapping("/data")
